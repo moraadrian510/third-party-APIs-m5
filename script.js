eec7d26245d8f8cbceee54219c1760b0f0fcc366
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-01-18 12:27:41 -0800

    timeBlock update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-
+$(document).ready(function () {
 
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -10,12 +10,42 @@
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
     //
+$('.saveBtn').on('click', handleSave);
+function handleSave() {
+    let plan = $(this).siblings('.description').val();
+    let hour = $(this).parent().attr('id');
+
+    localStorage.setItem(hour, plan);
+}
+
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
     //
+function applyColor() {
+    let currentTime =  dayjs().hour();
+    console.log('current time: ' + currentTime);
+
+    $('.time-block').each(function () {
+        let planHour = parseInt($(this).attr('id').split('-')[1]);
+
+        if (planHour < currentTime) {
+            $(this).addClass('past');
+        } else if (planHour === currentTime) {
+            $(this).removeClass('past');
+            $(this).addClass('present');
+        } else {
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+            $(this).addClass('future');
+        }
+        });
+    
+}
+
+applyColor();
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
@@ -31,5 +61,5 @@ var today = dayjs();
 }
 
 
-
+});
  
