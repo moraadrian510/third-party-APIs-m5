999427781ff880353e080f2762135f85d88d63b5
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-01-18 12:36:13 -0800

    added local storage memory

--- script.js
+++ script.js
@@ -50,7 +50,17 @@ applyColor();
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
     //
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
     // DONE: Add code to display the current date in the header of the page field
+
 
 // sets and refreshes time in header
 setInterval(setTime, 1000);
